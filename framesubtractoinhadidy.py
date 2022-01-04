704c32b03dc8814a9b7774c522c15a2fdedd0a99
Mohamed Khaled Mohamed <56936494+MohamedElhadidy0019@users.noreply.github.com>
2022-01-05 01:03:20 +0200

    lil code

--- framesubtractoinhadidy.py
+++ framesubtractoinhadidy.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from numpy.core.arrayprint import printoptions
+import skimage.io as io
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
@@ -11,11 +13,17 @@ if (cap.isOpened()== False):
 
 picList=[]
 # Read until video is completed
+i=200
 while(cap.read()):
   # Capture frame-by-frame
+  i-=1
+  if(i==0):
+    break
+
   ret, frame = cap.read()
   if ret == True:
-
+    if(i==2):
+      print(frame.shape)
     picList.append(frame)
     # Display the resulting frame
     #cv2.imshow('Frame',frame)
@@ -37,4 +45,18 @@ while(cap.read()):
 arr = np.asarray(picList)
 
 print(arr.shape)
+
+meanimage=np.sum(arr,axis=0)//len(picList)
+print(meanimage.shape)
+io.imshow(meanimage)
+print(meanimage)
+#cv2.imshow(meanimage)
+cv2.imshow("lol", meanimage)
+  
+# waits for user to press any key
+# (this is necessary to avoid Python kernel form crashing)
+cv2.waitKey(0)
+  
+# closing all open windows
+cv2.destroyAllWindows()
 
